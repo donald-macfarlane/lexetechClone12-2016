6fcc50bb7fd095d53609a487b4082a44a200e68d
Josh Chisholm <joshuachisholm@gmail.com>
2014-11-28 17:23:35 +0000

    gulp server bundles first

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,28 @@ var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 var browserify = require('browserify');
 
+var bundler = watchify(browserify('./app/app.pogo', {
+  cache: {},
+  packageCache: {},
+  fullPaths: true,
+  extensions: ['.pogo']
+}));
+bundler.transform('pogoify');
+bundler.on('update', rebundle);
+
+function rebundle() {
+  return bundler.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./public'));
+}
+
 gulp.task('default', ['watch', 'server'])
 
-gulp.task('watch', function() {
-  var bundler = watchify(browserify('./app/app.pogo', {cache: {}, packageCache: {}, fullPaths: true, extensions: ['.pogo']}));
-  bundler.transform('pogoify');
-  bundler.on('update', rebundle);
-
-  function rebundle() {
-    return bundler.bundle()
-      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-      .pipe(source('app.js'))
-      .pipe(gulp.dest('./public'));
-  }
-
-  return rebundle();
-});
+gulp.task('watch', rebundle);
 
 gulp.task('server', function() {
+  rebundle();
   gulp.src('public')
     .pipe(webserver({
       livereload: true,
