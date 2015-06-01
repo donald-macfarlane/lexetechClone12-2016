65b0877b61a96e0e0fd9ddd6113e0533baffd372
Tim Macfarlane <timmacfarlane@gmail.com>
2015-06-01 12:28:50 +0200

    validation on email address.

--- browser/form.js
+++ browser/form.js
@@ -2,6 +2,10 @@ var h = require('plastiq').html;
 var semanticUi = require('plastiq-semantic-ui');
 
 module.exports = {
+  form: function (options, settings, vdom) {
+    return semanticUi.form(options, settings, vdom);
+  },
+
   text: function (label, binding, options) {
     return this.field(label, binding, options);
   },
@@ -34,11 +38,12 @@ module.exports = {
     var type = options && options.hasOwnProperty('type') && options.type !== undefined? options.type: 'text';
     var placeholder = options && options.hasOwnProperty('placeholder') && options.placeholder !== undefined? options.placeholder: 'text';
     var _class = options && options.hasOwnProperty('class') && options.class !== undefined? options.class: undefined;
+    var name = options && options.hasOwnProperty('name') && options.name !== undefined? options.name: undefined;
 
     return h('.field', {class: _class},
       h('label', label),
       h('.ui.input',
-        h('input', {type: type, placeholder: placeholder, binding: binding})
+        h('input', {type: type, placeholder: placeholder, binding: binding, name: name})
       )
     );
   }
--- browser/userComponent.js
+++ browser/userComponent.js
@@ -12,6 +12,7 @@ module.exports = prototype({
   constructor: function (user) {
     this.user = user;
     this.userApi = userApi();
+    this.validationPromises = 0;
 
     this.getResetPasswordToken = throttle(function (user) {
       if (!user.hasPassword && user.id) {
@@ -29,11 +30,21 @@ module.exports = prototype({
 
     this.getResetPasswordToken(this.user);
 
-    function saveUser() {
-      delete self.user.dirty;
-      return self.user.save().then(function (user) {
-        routes.adminUser({userId: user.id}).push();
+    function saveUser(formElement) {
+      var promise = self.validation.then(function () {
+        delete self.user.dirty;
+        delete self.validation;
+
+        return self.user.save().then(function (user) {
+          routes.adminUser({userId: user.id}).push();
+        });
+      }, function () {
+        delete self.validation;
       });
+
+      formElement.form('validate form');
+
+      return promise;
     }
 
     function dirtyUser(v) {
@@ -46,36 +57,68 @@ module.exports = prototype({
     var user = this.user;
     var tokenLink = this.resetPasswordToken? location.origin + routes.resetPassword({token: this.resetPasswordToken}).href: undefined;
 
-    return h('form.ui.form.user',
-      newUser
-        ? h('h2', 'New User')
-        : h('h2',
-          this.user.firstName, ' ', this.user.familyName,
-          tokenLink
-            ? [
-                h('input.token-link', {type: 'text', value: tokenLink, onclick: function () { this.select(); }}),
-                zeroClipboard(
-                  tokenLink,
-                  h('.ui.button.green', 'copy')
-                )
-              ]
-            : h('span.no-token-link')
-        ),
-      h('.two.fields',
-        form.text('First Name', [this.user, 'firstName', dirtyUser], {class: 'first-name', placeholder: 'first name'}),
-        form.text('Family Name', [this.user, 'familyName', dirtyUser], {class: 'family-name', placeholder: 'family name'})
-      ),
-      form.text('Email', [this.user, 'email', dirtyUser], {class: 'email', placeholder: 'email'}),
-      form.textarea('Address', [this.user, 'address', dirtyUser]),
-      h('.two.fields',
-        form.text('Office Phone Number', [this.user, 'officePhoneNumber', dirtyUser], {placeholder: 'office phone number'}),
-        form.text('Cell Phone Number', [this.user, 'cellPhoneNumber', dirtyUser], {placeholder: 'cell phone number'})
-      ),
-      form.text('State License Number', [this.user, 'stateLicenseNumber', dirtyUser], {placeholder: 'state license number'}),
-      form.boolean('Author', [this.user, 'author', dirtyUser]),
-      form.boolean('Admin', [this.user, 'admin', dirtyUser]),
-      h('.ui.button', {class: {disabled: !this.user.dirty, blue: !newUser, green: newUser}, onclick: saveUser}, newUser? 'Create': 'Save'),
-      h('.ui.button', {onclick: routes.admin().push}, 'Close')
+    if (!this.validation) {
+      this.validationPromises++;
+      this.validation = new Promise(function (fulfil, reject) {
+        var x = self.validationPromises;
+        self.onSuccess = fulfil;
+        self.onFailure = reject;
+      });
+    }
+
+    return form.form(
+      {
+        email: {
+          identifier: 'email',
+          rules: [{
+            type: 'email',
+            prompt: 'please enter a valid email address'
+          }]
+        }
+      },
+      {
+        onSuccess: function () {
+          return self.onSuccess.apply(self, arguments);
+        },
+        onFailure: function () {
+          return self.onFailure.apply(self, arguments);
+        }
+      },
+      function (component) {
+        var formElement = component.state.formElement;
+
+        return h('form.ui.form.user',
+          newUser
+            ? h('h2', 'New User')
+            : h('h2',
+              self.user.firstName, ' ', self.user.familyName,
+              tokenLink
+                ? [
+                    h('input.token-link', {type: 'text', value: tokenLink, onclick: function () { this.select(); }}),
+                    zeroClipboard(
+                      tokenLink,
+                      h('.ui.button.green', 'copy')
+                    )
+                  ]
+                : h('span.no-token-link')
+            ),
+          h('.two.fields',
+            form.text('First Name', [self.user, 'firstName', dirtyUser], {class: 'first-name', placeholder: 'first name'}),
+            form.text('Family Name', [self.user, 'familyName', dirtyUser], {class: 'family-name', placeholder: 'family name'})
+          ),
+          form.text('Email', [self.user, 'email', dirtyUser], {class: 'email', placeholder: 'email', name: 'email'}),
+          form.textarea('Address', [self.user, 'address', dirtyUser]),
+          h('.two.fields',
+            form.text('Office Phone Number', [self.user, 'officePhoneNumber', dirtyUser], {placeholder: 'office phone number'}),
+            form.text('Cell Phone Number', [self.user, 'cellPhoneNumber', dirtyUser], {placeholder: 'cell phone number'})
+          ),
+          form.text('State License Number', [self.user, 'stateLicenseNumber', dirtyUser], {placeholder: 'state license number'}),
+          form.boolean('Author', [self.user, 'author', dirtyUser]),
+          form.boolean('Admin', [self.user, 'admin', dirtyUser]),
+          h('.ui.button', {class: {disabled: !self.user.dirty, blue: !newUser, green: newUser}, onclick: function () { return saveUser(formElement); }}, newUser? 'Create': 'Save'),
+          h('.ui.button', {onclick: routes.admin().push}, 'Close')
+        );
+      }
     );
   }
 });
