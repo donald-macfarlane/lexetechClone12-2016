9e7abe606eb145c0300659f15ac23164f806b3a8
Tim Macfarlane <timmacfarlane@gmail.com>
2015-08-14 14:17:55 +0200

    using hash for routing

--- browser/app.js
+++ browser/app.js
@@ -2,7 +2,7 @@ window.lexemeDebug = require('debug');
 var plastiq = require('plastiq');
 var router = require('plastiq-router');
 
-router.start();
+router.start({history: router.hash});
 
 var root = require('./rootComponent')(window.lexemeData);
 plastiq.append(document.body, root.render.bind(root));
