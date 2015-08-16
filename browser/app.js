20237e19b1e0c5316de240fe0f3ce856672b0741
Tim Macfarlane <timmacfarlane@gmail.com>
2015-08-16 14:42:20 +0200

    switching between ie and other support

--- browser/app.js
+++ browser/app.js
@@ -1,8 +1,17 @@
-window.lexemeDebug = require('debug');
+window._debug = require('debug');
 var plastiq = require('plastiq');
 var router = require('plastiq-router');
 
-router.start({history: router.hash});
+if (!history.pushState) {
+  if (location.pathname == '/') {
+    router.start({history: router.hash});
+  } else {
+    var path = (location.pathname + location.search).replace(/^\//, '');
+    location.href = '/#' + path;
+  }
+} else {
+  router.start();
+}
 
 var root = require('./rootComponent')(window.lexemeData);
 plastiq.append(document.body, root.render.bind(root));
